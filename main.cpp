025195a46440933cadbd972f21f02117d6c5ff15
AndrewAZah <andrew@roguesurge.com>
2026-03-11 19:59:14 -0700

    A32 done

--- main.cpp
+++ main.cpp
@@ -6,10 +6,13 @@ int main()
   int numFemale, numMale, numOthers;
   double percF, percM, percO;
 
-  cout << "Enter the number of students: Male, Female and Others";
+  cout << "Enter the number of students: Male, Female and Others" << endl;
   cin >> numMale >> numFemale >> numOthers;
   // TODO
-
+  int total = numMale + numFemale + numOthers;
+  percM = double(numMale) / total * 100;
+  percF = double(numFemale) / total * 100;
+  percO = double(numOthers) / total * 100;
   cout << setprecision(2) << fixed;
   cout << "Percentage of Male: " << percM << endl;
   cout << "Percentage of Female: " << percF << endl;
